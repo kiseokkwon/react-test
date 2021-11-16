f823a155e5d4081b799ebf9710d81e51ed964f8d
kisskwon <kisskwon87@gmail.com>
2021-11-16 22:59:10 +0900

    file download

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    axios({
+      url: "http://demo.borland.com/testsite/downloads/downloadfile.php?file=MyDocument.docx&cd=attachment+filename", //your url
+      method: 'GET',
+      responseType: 'arraybuffer',
+    }).then(response => {
+      console.log(response);
+      console.log(response.headers["content-type"]);
+      
+      const url = window.URL.createObjectURL(new Blob([response.data], {type: response.headers["content-type"]}));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'file.docx');
+      document.body.appendChild(link);
+      link.click();
+    }).catch(error => {
+      console.log(error);
+    });
+    return () => {
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
